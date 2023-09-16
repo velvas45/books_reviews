e813c430cf5a1f6c6c6cb6808a19644a9cdff1d8
velvas45 <helmiagustiawan905@gmail.com>
2023-09-16 20:51:13 +0700

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Cache;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
+
+        $books = Book::when($title, fn ($query, $title) =>  $query->title($title));
+
+        $books = match($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()->withAvgRating()->withReviewsCount()
+        };
+        // $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = 
+            cache()->remember(
+                $cacheKey, 
+                3600, 
+                fn()=> $books->get()
+            ); 
+
+
+        // return view('books.index', compact('books'));
+        // compact function yg disediakan laravel untuk mencari variabel yg dimasukan keparameternya dan menjadikannya array dengan index key dan value yg sama. contohnya spt di bawah.
+        return view('books.index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {   
+        $cacheKey = 'book:' . $id;
+
+        $book = cache()->remember(
+            $cacheKey, 
+            3600, 
+            fn() => Book::with([
+            'reviews' => fn($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
+
+        return view(
+            'books.show', 
+            ['book' => $book]
+            // ['book' => $book->load([
+            //     // load diatas berguna untuk mengatur data relationship seperti mengurutkan datanya dari yg terbaru atau yang lainnya (https://laravel.com/docs/10.x/eloquent-relationships#lazy-eager-loading). 
+            //     'reviews' => fn ($query) => $query->latest() 
+            // ])]
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
